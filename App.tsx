e398e01fb664c5a149499dff4b75505255455a43
shashankk748 <shashank.m748@gmail.com>
2023-12-07 22:12:13 +0530

    added files

---
+++ App.tsx
@@ -0,0 +1,35 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+
+import {
+  Colors,
+  DebugInstructions,
+  Header,
+  LearnMoreLinks,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+
+function App(): React.JSX.Element {
+  return (
+    <View>
+      <Text>Hey there whats up</Text>
+    </View>
+  );
+}
+
+export default App;
